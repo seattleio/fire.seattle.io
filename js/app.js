5176012a9a5fa32fd2e3d22c76c1245509ed6d9f
Peter Breen <pbreen@gmail.com>
2017-02-22 11:42:10 -0800

    changes API call to use updated endpoint; $.each() function updated to work with json endpoint

--- js/app.js
+++ js/app.js
@@ -1,22 +1,28 @@
-$.getJSON('http://data.seattle.gov/api/views/kzjm-xkqj/rows.json?jsonp=?&max_rows=25', function(results) {
-  console.log(results.data);
-
-  var map = L.map('map').setView([47.6097, -122.3331], 11);
-
-  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+$.ajax({
+  url: 'https://data.seattle.gov/resource/grwu-wqtk.json?$limit=25',
+  method: 'GET',
+  dataType: 'json',
+  // data: {
+  //   'codedescription': 'EMBEZZLEMENT',
+  //   '$$app_token': app_token
+  // },
+  success: function( data, status, jqxhr ){
+    console.log(Object.values(data));
+    var map = L.map('map').setView([47.6097, -122.3331], 11);
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-  }).addTo(map);
+    }).addTo(map);
 
-  $.each(results.data, function(index, value) {
-    if (value[11] && value[12]) {
-      var date = new Date(value[10]*1000);
-      var moment_date = moment(date).format("h:mm a, MMM D, YYYY");
-
-      L.marker([value[11], value[12]]).addTo(map)
-      .bindPopup('<h6>' + value[9] + '</h6>' + value[8] + '<br>' + moment_date );
-    }
-  });
-
+    $.each(data, function() {
+      if (this.latitude && this.longitude) {
+        var date = new Date(this['datetime']);
+        var moment_date = moment(date).format('h:mm a, MMM D, YYYY');
+        L.marker([this['latitude'], this['longitude']]).addTo(map)
+        .bindPopup('<h6>' + this['type'] + '</h6>' + this['address'] + '<br>' + moment_date );
+      }
+    });
+  },
+  error: function( jqxhr, status, error ){
+    console.log( 'Something went wrong!' );
+  }
 });
-
-
